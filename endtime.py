6236c1c3b17f25007edff1a2c038c278ea1722d5
Mamajonov Ibrohimjon <mamajonovibrokhimjon@gmail.com>
2023-10-21 20:48:02 +0500

    T4

---
+++ endtime.py
@@ -0,0 +1,18 @@
+class TugashVaqti:
+    def __init__(self, hour, minut, second, ms) -> None:
+        self._hour = hour
+        self._minut = minut
+        self._second = second
+        self._ms = ms
+
+class Time:
+    def __init__(self, gp, driver, tugash_vaqti) -> None:
+        self._gp = gp
+        self._driver = driver
+        self._tugash_vaqti = tugash_vaqti
+
+    def __str__(self) -> str:
+        vaqt = self._tugash_vaqti
+        return f"{vaqt._hour}:{vaqt._minut}:{vaqt._second}.{vaqt._ms}"
+        
+
--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from chempionship import Championship
+from gp import GP
+from haydovchi import Haydovchi
+from endtime import TugashVaqti
 
 chempionat = Championship()
+
+chempionat.defineGrandPrix("katta halqa")
+gp = GP("katta halqa")
+gp1 = GP("tekis yolak")
+driver1 = Haydovchi('ali')
+driver2 = Haydovchi('vali')
+
+tugash_vaqti1 = TugashVaqti(11, 35, 34, 123)
+tugash_vaqti2 = TugashVaqti(11, 35, 34, 122)
+tugash_vaqti3 = TugashVaqti(11, 35, 34, 120)
+
+
+print(chempionat.set_time(gp, driver1, tugash_vaqti1))
+print(chempionat.set_time(gp, driver2, tugash_vaqti2))
+print(chempionat.set_time(gp1, driver1, tugash_vaqti3))
+
+a = chempionat.getGrandPrix("katta halqa")
+print(a.get_gp_ranking())
+print(a.get_position(driver1))
+print(driver1.get_raced())
 
 # print(chempionat.defineGrandPrix('katta aylana yo\'li'))
 # print(chempionat.getGrandPrix('kattaaylana yo\'li'))
